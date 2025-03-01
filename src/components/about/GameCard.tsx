b03c75b3178f01ad9795b1822fb138c967c06022
Daniel Mamuza <94477022+MamuzaD@users.noreply.github.com>
2025-02-28 23:39:21 -0800

    update games retrieval && image size

--- src/components/about/GameCard.tsx
+++ src/components/about/GameCard.tsx
@@ -6,20 +6,26 @@ interface GameCardProps {
 }
 
 const GameCard = ({ game }: GameCardProps) => {
+  const text = game.playtime_2weeks
+    ? `i've played ${
+        game.playtime_2weeks < 120 ? `${game.playtime_2weeks} mins` : `${(game.playtime_2weeks / 60.0).toFixed(1)} hrs`
+      } recently`
+    : `i've played ${(game.playtime_forever / 60.0).toFixed(1)} hrs total`
+
   return (
     <HoverCard openDelay={500} closeDelay={50}>
       <HoverCardTrigger>
         <img
-          width={374}
-          height={488}
-          src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/hero_capsule.jpg`}
+          width={80}
+          height={80}
+          src={`https://images.weserv.nl/?url=https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/hero_capsule.jpg&w=300&h=300&fit=cover`}
           alt={`${game.name}'s Picture`}
           style={{ imageRendering: "crisp-edges" }}
           className="no-sound h-16 w-16 rounded-lg object-cover transition-transform duration-300 ease-in-out hover:scale-110 md:h-[4.75rem] md:w-[4.75rem]"
         />
       </HoverCardTrigger>
       <HoverCardContent
-        className="z-999 flex h-40 w-full flex-col place-items-center justify-between bg-primary-foreground/80 backdrop-blur-xl"
+        className="bg-primary-foreground/80 z-999 flex h-40 w-full flex-col place-items-center justify-between backdrop-blur-xl"
         side="top"
       >
         <img
@@ -30,11 +36,7 @@ const GameCard = ({ game }: GameCardProps) => {
           style={{ imageRendering: "crisp-edges" }}
           className="w-full rounded-lg object-cover transition-transform duration-300 ease-in-out hover:scale-110"
         />
-        <span className="font-semibold">{`i've played ${
-          game.playtime_2weeks < 120
-            ? `${game.playtime_2weeks} mins`
-            : `${(game.playtime_2weeks / 60.0).toFixed(1)} hrs`
-        } recently`}</span>
+        <span className="font-semibold">{text}</span>
       </HoverCardContent>
     </HoverCard>
   )
