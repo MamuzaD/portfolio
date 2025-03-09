8bf08cc6e19228138cfb5e04434786e35dac8b93
Daniel Mamuza <94477022+MamuzaD@users.noreply.github.com>
2025-03-08 23:08:44 -0800

    click up contact trigger

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -23,6 +23,7 @@ export default function Contact() {
   const [copied, setCopied] = useState(false)
   const [copiedMessage, setCopiedMessage] = useState<string>("")
   const formRef = useRef<HTMLDivElement | null>(null)
+  const dropdownRef = useRef<HTMLDivElement>(null)
 
   const handleCopy = (name: string, href: string) => {
     navigator.clipboard.writeText(href)
@@ -35,58 +36,73 @@ export default function Contact() {
     if (viewForm && formRef.current) formRef.current.scrollIntoView({ behavior: "smooth", block: "start" })
   }, [viewForm])
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node) && dropdownOpen)
+        setDropdownOpen(false)
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [dropdownOpen])
+
   return (
     <section className="z-50 flex flex-col items-center">
       {!viewForm && (
-        <DropdownMenu modal={false} open={dropdownOpen}>
-          <DropdownMenuTrigger
-            onClick={() => setDropdownOpen((val) => !val)}
-            className={`min-h-26 z-50 text-4xl font-bold ${!copied && "pb-5"} hover-underline-animation cursor-pointer`}
-          >
-            contact me
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-full bg-muted/50 backdrop-blur-md">
-            <DropdownMenuLabel className="text-center">your preference</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              onClick={() => {
-                setViewForm(true)
-              }}
-              className="flex cursor-pointer items-center gap-2 text-lg"
+        <div ref={dropdownRef}>
+          <DropdownMenu modal={false} open={dropdownOpen}>
+            <DropdownMenuTrigger
+              onClick={() => setDropdownOpen((val) => !val)}
+              className={`min-h-26 z-50 text-4xl font-bold ${!copied && "pb-5"} hover-underline-animation cursor-pointer`}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2M7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7zm10 2h-6v-2h6zm0-4h-6v-2h6zm0-4h-6V7h6z"
-                />
-              </svg>
-              <span>contact form</span>
-            </DropdownMenuItem>
-            {reachouts.map((r) => (
-              <DropdownMenuItem key={r.name} className="items-center justify-between gap-10 text-lg">
-                <div className="flex items-center gap-2">
-                  {r.icon}
-                  <span>{r.name}</span>
-                </div>
-                <div className="flex gap-1">
-                  <Button size="icon" variant="outline" className="size-9 dark:hover:bg-primary-foreground">
-                    <a href={r.href} target="_blank" rel="noopener noreferrer">
-                      <Link2 />
-                    </a>
-                  </Button>
-                  <Button
-                    size="icon"
-                    variant="outline"
-                    className="size-9 dark:hover:bg-primary-foreground"
-                    onClick={() => handleCopy(r.name, r.href)}
-                  >
-                    <Clipboard />
-                  </Button>
-                </div>
+              contact me
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-full bg-muted/50 backdrop-blur-md">
+              <DropdownMenuLabel className="text-center">your preference</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={() => {
+                  setViewForm(true)
+                  setDropdownOpen(false)
+                }}
+                className="flex cursor-pointer items-center gap-2 text-lg"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2M7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7zm10 2h-6v-2h6zm0-4h-6v-2h6zm0-4h-6V7h6z"
+                  />
+                </svg>
+                <span>contact form</span>
               </DropdownMenuItem>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
+              {reachouts.map((r) => (
+                <DropdownMenuItem key={r.name} className="items-center justify-between gap-10 text-lg">
+                  <div className="flex items-center gap-2">
+                    {r.icon}
+                    <span>{r.name}</span>
+                  </div>
+                  <div className="flex gap-1">
+                    <Button size="icon" variant="outline" className="size-9 dark:hover:bg-primary-foreground">
+                      <a href={r.href} target="_blank" rel="noopener noreferrer">
+                        <Link2 />
+                      </a>
+                    </Button>
+                    <Button
+                      size="icon"
+                      variant="outline"
+                      className="size-9 dark:hover:bg-primary-foreground"
+                      onClick={() => handleCopy(r.name, r.href)}
+                    >
+                      <Clipboard />
+                    </Button>
+                  </div>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       )}
       {viewForm && (
         <div ref={formRef}>
